cef7f3d89d4b0b0f67e7b8dce6baa7a2ad097c72
dbn <dimabur@gmail.com>
2025-09-20 18:18:14 +0300

    build problems

--- src/disney/shared/config.py
+++ src/disney/shared/config.py
@@ -2,8 +2,8 @@
 
 import os
 from typing import Optional
-from pydantic import BaseSettings, Field
-
+from pydantic import  Field
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     """Application settings."""
